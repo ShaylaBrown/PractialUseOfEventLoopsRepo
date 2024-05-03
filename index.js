9e10144a6cfc95cbcc00caca62182dd6b2310bcd
Shayla Brown <shaylabrown227@gmail.com>
2024-05-03 14:07:13 -0500

    finished homework

--- index.js
+++ index.js
@@ -16,21 +16,21 @@ try {
 
 // This function will take a number and incremnt conuter to that number
 
- function incrementTo(num) {
-   if (num <= 0) return;
+function incrementTo(num) {
+    if (num <= 0) return;
 
-   if (num === counter) return;
+    if (num === counter) return;
 
-   counter += 1;
-   incrementTo(num);
- }
+    counter += 1;
+    incrementTo(num);
+}
 
- try {
-   incrementTo(100);
-   console.log(counter);
- } catch (err) {
-   console.log(err);
- }
+try {
+    incrementTo(100);
+    console.log(counter);
+} catch (err) {
+    console.log(err);
+}
 
 //Part 2.a :recursive function that completely flattens a nested array
 
@@ -45,4 +45,23 @@ const numbers = [1, 2, 3, [4, 5, [6, 7], 8], 9];
 console.log(flattenArray(numbers));
 // console.log(trampoline(numbers)); This line of code is not working.
 
-//Part 2.b : trampoline recursive function
+//Part 3 : Deferred Execution
+const pTag = document.getElementById("simple text")
+
+function isPrime(num) { 
+    for (let i = 2; i <= Math.sqrt(num); i++) { 
+        if (num % i === 0) { 
+            return false; 
+        } 
+    } 
+    return num > 1; 
+} 
+  
+function primeNumber(n) {
+    for (let i = 2; i <= n; i++) {
+        if (isPrime(i)) {
+            console.log(i);
+        }
+    }
+}
+primeNumber(100);
